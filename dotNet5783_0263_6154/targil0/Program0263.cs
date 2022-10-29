00b701684413983187038bc32ce66d9f3627b3c7
Noa-Lubin <noal5949@gmail.com>
2022-10-29 22:01:56 +0300

    Initial code + welcome functions

---
+++ dotNet5783_0263_6154/targil0/Program0263.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace targil0
+{
+    partial class Program
+    {
+        static void main()
+        {
+            Welcome0263();
+            Welcome6154();
+            Console.ReadKey();
+        }
+        static partial void Welcome6154();
+
+        private static void Welcome0263()
+        {
+            Console.Write("Enter your name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", userName);
+        }
+    }
+}
+
